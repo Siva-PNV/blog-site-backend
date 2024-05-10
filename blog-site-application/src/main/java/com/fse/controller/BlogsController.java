6c4dc0f526e8bf990834d73198aa303144f21f5e
Siva Pallem <siva.pallem@rabobank.com>
2024-05-10 11:24:15 +0530

    added git ignore file
    added git ignore file

--- blog-site-application/src/main/java/com/fse/controller/BlogsController.java
+++ blog-site-application/src/main/java/com/fse/controller/BlogsController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -20,6 +21,7 @@ import com.fse.services.UsersService;
 
 @RestController
 @RequestMapping("/api/v1.0/blogsite/blogs/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BlogsController {
 
 	@Autowired
