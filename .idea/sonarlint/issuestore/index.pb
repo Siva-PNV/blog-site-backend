a572c8f61327bf32e19e595b73d8fe9af8962f88
Siva Pallem <siva.pallem@rabobank.com>
2024-06-04 11:34:33 +0530

    updates
    updates

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,9 +7,5 @@ c
 3src/test/java/com/fse/BlogSiteApplicationTests.java,1/d/1dc19d20fb75cb40a38ca48bf71d7fe8f3fb371a
 e
 5src/test/java/com/fse/controller/BlogsController.java,4/3/43a35af61d0765ebe0d3f0cc9647e2caf6896472
-^
-.src/main/java/com/fse/BlogSiteApplication.java,8/6/86f4ab70f6a4f5876b7da1057f158ac076cf542f
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
-k
-;src/main/java/com/fse/auth/JwtAuthenticationEntryPoint.java,4/5/456c76f5c3b0a462f9bc959d0ade295cdd45ad91
--- src/main/java/com/fse/BlogSiteApplication.java
+++ src/main/java/com/fse/BlogSiteApplication.java
@@ -2,12 +2,25 @@ package com.fse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages="com.fse")
 public class BlogSiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogSiteApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://blogs-site.azurewebsites.net/");
+			}
+		};
 	}
 
 }
